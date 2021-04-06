200fe83445e8cbc2453859cd852e0f8294acb5a9
Kora <korastuff@gmail.com>
2021-04-06 11:45:46 -0400

    Xcode Project for cpp compilation

---
+++ 21_Spring/AStarNewVersions/SearchAlgorithm/SearchAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SearchAlgorithm
+//
+//  Created by Kora Hughes on 4/6/21.
+//  Copyright © 2021 NYU Augmented Library VIP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
